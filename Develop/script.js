393778377d087e57024af6ce0da86a954498a400
Amanda Jorgensen <amanda.jorgensen.m@gmail.com>
2022-09-15 12:23:51 -0400

    reworked entire javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,55 +1,77 @@
-// Get references to the #generate element
-// this targets the button 
+// Step1:
+// Get references to the #generate element. This targets the button.
 var generateBtn = document.querySelector("#generate"); 
 
-// Assignment code here
-var lengthOfCharacters = []; 
+var passwordLength = 8; 
 var choiceArray = [];
 
-var upperCaseLetterslist = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowerCaseLetterlist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var numericlist = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var symbolslist = ['*', '$', '#', '@', '%']
+// array of options
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var symbols = ['*', '$', '#', '@', '%']
 
-
-// Define the function to generate the password
-function generatePassword() {
-// Prompt the user for the password criteria
-// Length between 8 < 128
-var userInput = window.prompt("How long do you want your password to be?")
-
-var length = parseInt("userInput")
-
-if (length < 8 || length > 128){
-  window.alert("Password length must be between 8 and 128 characters long.")
-  return
-}
-
-/* if (isNaN(length)) {
-  window.alert("This is not a number.")
-  return */
-} 
-// Lowercase, uppercase, special characters, numbers
-var numeric = window.confirm("Would you like to include numbers?")
-var upperCaseLetters = window.confirm("Would you like to include uppercase letters?")
-var lowerCaseLetters = window.confirm("Would you like to include lowercase letters?")
-var symbols = window.confirm("Would you like to include symbols?")
-
-// Validate the input
-// Generate password based on criteria
-// Display the password
-
-
-
-// Write password to the #password input
-// We are naming the password "writePassword", and storing it into the var password variable.
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+var randomizer = []
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input. We are naming the password function "writePassword", and storing it into the var password variable.
+function writePassword() {
+  var userInputPrompts = prompts();
+  var passwordText = document.querySelector("#password");
+  
+  if (userInputPrompts) {
+  var generatedPassword = generatePassword();
+  passwordText.value = generatedPassword;
+} else {
+  passwordText.value = "";
+}
+}
+
+// Define the function called generatePassword
+function generatePassword(){
+ var password = "";
+// integer starts at 0, must be greater than 8, increase every 1
+ for(var i = 0; i < passwordLength; i++) {
+  var randomPassword = Math.floor(Math.random() * choiceArray.length);
+  password = password + choiceArray[randomPassword];
+}
+return password;
+}
+
+// criteria prompts
+function prompts() {
+  choiceArray = [];
+
+//  Step 2: Validate the user input.
+  // parseInt is a method that turns any string into a number
+  characterLength = parseInt(prompt('How long do you want your password to be? Note: It must be between 8 and 128 characters long.'));
+//Check to make sure the password meets the criteria. 1) Needs to be a number. 2) Needs to be greater than 8. 3) Needs to be less than 128.
+  if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    window.alert("Incorrect input. Please try again.");
+    return false;
+  }
+  if (window.confirm('Would you like to include numbers?')) {
+    choiceArray = choiceArray.concat(numbers);
+  }
+
+  if (window.confirm('Would you like to include lowercase letters?')) {
+    choiceArray = choiceArray.concat(lowerCaseLetters);
+  }
+
+  if (window.confirm('Would you like to include uppercase letters?')) {
+    choiceArray = choiceArray.concat(upperCaseLetters);
+  }
+
+  if (window.confirm('Would you like to include symbols?')) {
+    choiceArray = choiceArray.concat(symbols);
+  }
+  // // if the list is empty, meaning the user doesn't choose anything, then push/add/default to lowercase and uppercase letters.
+  // if (choiceArray.Length === 0){
+  //   choiceArray.concat(lowerCaseLetters, upperCaseLetters);
+  // }
+// Step 3:
+// Display the generated password to the page.
+  return true;
+}
